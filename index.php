99cfcbff11cb63a185cf096b4388f495711035c8
Kimberly Le <91621873+lekimberly17@users.noreply.github.com>
2022-02-28 04:03:54 +0000

    changed the data from film to nhl

--- index.php
+++ index.php
@@ -1,21 +1,95 @@
 <html>
-    <head>
-        <title>PHP FIle!</title>
-    </head>
-<body>
-    <h1>PHP File!</h1>
+<head>
+<title>Bond Web Service Demo</title>
+<style>
+body {font-family:georgia;}
 
-    <p>This is HTML!</p>
+  .hockey{
+    border:1px solid #E77DC2;
+    border-radius: 5px;
+    padding: 5px;
+    margin-bottom:5px;
+    position:relative;   
+  }
+ 
+  .pic{
+    position:absolute;
+    right:10px;
+    top:10px;
+  }
 
-    <?php
+</style>
+<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
-    echo "<p>This is PHP!</p>";
+<script type="text/javascript">
 
-    ?>
+function hockeyTemplate(team){
+   return `<div class="hockey">
+      <b>Team: </b> ${team.Team}<br />
+      <b>GamesPlayed: </b> ${team.GamesPlayed}<br />
+      <b>Loses: </b> ${team.Loses}<br />
+      <b>Wins: </b> ${team.Wins}<br />
+      <b>OverTimeLoss: </b> ${team.OverTimeLoss}<br />
+      <b>Points: </b> ${team.Points}<br />
+      <b>Streaks: </b> ${team.Streaks}<br />
+      <div class="pic"><img src="thumbnails/${team.Image}" /></div>
+    </div>`;
+}
 
-    <p>This is HTML!</p>
+$(document).ready(function() {  
 
-  <p>Hello from repl.it!</p>
-</body>
+	$('.category').click(function(e){
+    e.preventDefault(); //stop default action of the link
+		cat = $(this).attr("href");  //get category from URL
 
+    var request = $.ajax({
+      url: "api.php?cat=" + cat,
+      method: "GET",
+      dataType: "json"
+    });
+    request.done(function( data ) {
+      console.log(data);
+      // $("#hockeytitle").html(data.title);
+
+      //clears the previous films
+      $("#hockeys").html("");
+      //loops through films 
+      $.each(data.teams,function(key,value){
+      let str = hockeyTemplate(value);
+
+      $("<div></div>").html(str).appendTo("#hockeys");
+        
+      });
+
+      //Place the title of the web service on the page
+      //$("#output").text(JSON.stringify(data));
+      
+      let myData = JSON.stringify(data,null,4);
+
+      myData = "<pre>" + myData + "</pre>";
+      $("#output").html(myData);
+      
+      
+    });
+    request.fail(function(xhr, status, error) {
+      //Ajax request failed.
+      var errorMessage = xhr.status + ': ' + xhr.statusText
+      alert('Error - ' + errorMessage);
+    });
+	});
+});	
+
+</script>
+</head>
+	<body>
+	<h1>Bond Web Service</h1>
+		<a href="alpha" class="category">National Hockey League</a><br />
+		<!-- <a href="box" class="category">Bond Films By International Box Office Totals</a> -->
+		<h3 id="hockeytitle">Title Will Go Here</h3>
+		<div id="hockeys">
+			<p>League will go here</p>
+		</div>
+
+		<div id="output">Results go here</div>
+	</body>
 </html>
